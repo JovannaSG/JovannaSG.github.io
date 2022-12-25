d5b945ac2ded153e9729222cbc74a1411f44a44f
JovannaSG <67232473+JovannaSG@users.noreply.github.com>
2022-12-25 18:17:34 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//<script type="text/javascript" src="name of file.js"></script>
+
+let userName = prompt('Введите ваше имя:', '');
+let userEmail = prompt('Введите ваш email:', '');
+
+console.log(userName);
+console.log(userEmail);
